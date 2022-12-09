d46198d3494c8ab3469c26e46d7c73bc022e5c58
Patrick I <puut4991@yahoo.com>
2022-12-09 09:45:59 -0600

    Add HTML boilerplate & JS Psuedocode
    Basics needed for project planning beginning

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+Premise: Single-player rock paper scissors game against the computer.
+
+Make a function with no parameters
+    Return a random number between 1 and 3
+
+Make a function with no parameters
+    Make a variable to hold their choice
+    Prompt the user for either rock, paper, or scissors
+    Re-do if they input nothing or spell something wrong
+    Convert their choice into a 1, 2, or 3
+    Return their converted choice
+
+Make a function that takes the two choices from computer and player
+    Check for fail conditions for the computer
+    Check for win conditions for the computer
+    Check for a draw
+    Print the result
+
+Make a variable and call function1
+Make a variable and call function2
+Call function3 with results of functions 1 & 2
+*/
